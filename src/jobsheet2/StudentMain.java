d18fc8e5169d6fa79ac04d99ca145f68b1a42573
tiara082 <tiarafebrianie308@gmail.com>
2025-02-19 14:10:56 +0700

    Jobsheet 2 :  Expriment 2

--- src/jobsheet2/StudentMain.java
+++ src/jobsheet2/StudentMain.java
@@ -3,10 +3,15 @@ package jobsheet2;
 public class StudentMain {
     public static void main(String[] args) {
         Student25 student1 = new Student25();
+        
         student1.studentID= "244107020097";
         student1.name = "Tiara";
         student1.className = "TI-1I";
-        student1.updateGPA(90.0);
+        student1.updateGPA(3.8);
+        student1.print();
+
+        student1.changeClass("TI-2I");
+        student1.updateGPA(3.9);
         student1.print();
     }    
 }
